60b091513a8a61f0dcbf62183db6a52d028bd527
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2019-01-03 15:16:35 -1000

    support for defining a root directory for files

--- data/settings-example.py
+++ data/settings-example.py
@@ -10,6 +10,22 @@ endreplace = ''
 #the date format for the season date (default matches thetvdb.com)
 dateformat = '%Y-%m-%d'
 
+#the default title of the Kodi dialog box
+title = 'Available Streaming'
+
+#the default message in the Kodi dialog box
+msg = 'This video is available from a streaming service on another device'
+
+#the root of the video source for Kodi (minus the TV Show and Movie directories)
+#note that for Windows you need to put the path in the C:\\Users\\Public\\Videos'
+rootpath = None
+
+#the name of the directory where the TV Shows are stored
+tvroot = 'TV'
+
+#the name of the directory where Movies are stored
+movieroot = 'Movies'
+
 #if another instance of script is running, amount of time (in seconds) to wait before giving up
 aborttime = 30
 
@@ -18,4 +34,3 @@ logbackups = 1
 
 #for debugging you can get a more verbose log by setting this to True
 debug = False
-
--- data/settings.py
+++ data/settings.py
@@ -1,21 +1,6 @@
-#characters that the file system doesn't allow in a filename or that might confuse the Kodi scrapper
-illegalchars = '<>:"/\|?* '
-
-#what to use to replace illegal characters
-illegalreplace = '.'
-
-#what to use to replace trailing periods in a name (problem for folders on Windows)
-endreplace = ''
-
-#the date format for the season date (default matches thetvdb.com)
-dateformat = '%Y-%m-%d'
-
-#if another instance of script is running, amount of time (in seconds) to wait before giving up
-aborttime = 30
-
-#number of script logs to keep
-logbackups = 1
+#the root of the video source for Kodi (minus the TV Show and Movie directories)
+#note that for Windows you need to put the path in the C:\\Users\\Public\\Videos'
+rootpath = '/Users/pkscout/Documents/XBMC Test'
 
 #for debugging you can get a more verbose log by setting this to True
 debug = True
-
