99b7ba9c43a62c30ceeb8c4b6d7374d52d05d1ab
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2019-01-03 15:17:05 -1000

    removed settings file

--- data/settings.py
+++
@@ -1,6 +0,0 @@
-#the root of the video source for Kodi (minus the TV Show and Movie directories)
-#note that for Windows you need to put the path in the C:\\Users\\Public\\Videos'
-rootpath = '/Users/pkscout/Documents/XBMC Test'
-
-#for debugging you can get a more verbose log by setting this to True
-debug = True
