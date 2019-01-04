4c7acb74e829d4f75fcaa2edc3e17162ec67928f
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2019-01-03 14:35:08 -1000

    added ability to create stub files for movies

--- execute.py
+++ execute.py
@@ -1,14 +1,13 @@
 # *  Credits:
 # *
-# *  v.0.1.0
+# *  v.0.2.0
 # *  original Create Kodi Media Stubs code by pkscout
 
 import atexit, argparse, datetime, os, random, sys, time
 import resources.config as config
 from urllib.parse import urlencode, quote_plus
 from resources.common.xlogger import Logger
-from resources.common.fileops import deleteFile, readFile, writeFile, checkPath
-from resources.common.transforms import replaceWords
+from resources.common.fileops import deleteFile, writeFile, checkPath
 if sys.version_info < (3, 0):
     from ConfigParser import *
 else:
@@ -32,7 +31,7 @@ class Main:
         self._setPID()
         self._parse_argv()
         self._init_vars()
-        self._create_media_stubs()
+        self._create_stubs()
         
                 
     def _setPID( self ):
@@ -50,22 +49,33 @@ class Main:
 
     def _parse_argv( self ):
         parser = argparse.ArgumentParser()
-        parser.add_argument( "-s", "--series", required=True, help="the name of the series (required)" )
-        parser.add_argument( "-e", "--seasons", required=True, help="comma separated list of the seasons to create (required)" )
-        parser.add_argument( "-p", "--episodes", required=True, help="comma separated list of the number of episodes in each season (required)" )
+        parser.add_argument( "-n", "--name", help="the name of the series or movie" )
+        parser.add_argument( "-s", "--seasons", help="comma separated list of the seasons to create" )
+        parser.add_argument( "-e", "--episodes", help="comma separated list of the number of episodes in each season" )
         parser.add_argument( "-d", "--dates", help="comma separated list of season dates" )        
         parser.add_argument( "-t", "--title", help="title for the Kodi dialog box" )
         parser.add_argument( "-m", "--msg", help="message used in the Kodi dialog box" )
-        parser.add_argument( "-y", "--type", help="the media source for the stub (must be a valid Kodi type)" )
+        parser.add_argument( "-y", "--type", help="the media type for the stub (must be a valid Kodi type)" )
+        parser.add_argument( "-f", "--fromsettings", action="store_true", help="generate file based on airdate of episodes in settings file")
         parser.add_argument( "-r", "--streamfile", action="store_true", help="output as a stream file instead of a media stub")
         self.ARGS = parser.parse_args()
 
 
     def _init_vars( self ):
-        self.DATAROOT = os.path.join( p_folderpath, 'data' )
+        self.DATAROOT = config.Get( 'rootpath' )
+        if not self.DATAROOT:
+            self.DATAROOT = os.path.join( p_folderpath, 'data' )
         self.ILLEGALCHARS = list( config.Get( 'illegalchars' ) )
         self.ILLEGALREPLACE = config.Get( 'illegalreplace' )
         self.ENDREPLACE = config.Get( 'endreplace' )
+        if self.ARGS.title:
+            self.TITLE = self.ARGS.title
+        else:
+            self.TITLE = config.Get( 'title' )
+        if self.ARGS.msg:
+            self.MSG = self.ARGS.msg
+        else:
+            self.MSG = config.Get( 'msg' )
             
 
     def _add_leading_zeros( self, num ):
@@ -76,63 +86,64 @@ class Main:
             return str( num )
 
 
-    def _create_media_stubs( self ):
-        series_name = self._set_safe_name( self.ARGS.series )
-        season_list = self.ARGS.seasons.split( ',' )
-        episode_list = self.ARGS.episodes.split( ',' )
+    def _create_stubs( self ):
+        video_name = self._set_safe_name( self.ARGS.name )
+        video_path = os.path.join( self.DATAROOT, config.Get( 'tvroot' ), video_name )
+        try:
+            season_list = self.ARGS.seasons.split( ',' )
+            episode_list = self.ARGS.episodes.split( ',' )
+        except AttributeError as e:
+            video_path = os.path.join( self.DATAROOT, config.Get( 'movieroot' ), video_name )
+            season_list = []
+            episode_list = []
         if self.ARGS.dates:
             date_list = self.ARGS.dates.split( ',' )
         else:
             date_list = None
-        series_root = os.path.join( self.DATAROOT, series_name )
         if self.ARGS.type:
-            stub_type = '.' + self.ARGS.type
+            media_type = '.' + self.ARGS.type
         else:
-           stub_type = ''
+           media_type = ''
         file_text = self._get_file_text()
-        ref = 0
-        success, log_lines = checkPath( series_root )
+        success, log_lines = checkPath( video_path )
         lw.log( log_lines )
-        for season in season_list:
-            season_num = self._add_leading_zeros( season )
-            try:
-                max_eps = int( episode_list[ref] ) + 1
-            except IndexError:
-                break
-            for e in range( 1, max_eps):
-                ep_num = self._add_leading_zeros( e )
-                if self.ARGS.streamfile:
-                    ext = 'strm'
-                else:
-                    ext = 'disc'
-                file_name = '%s.S%sE%s%s.%s' % (series_name, season_num, ep_num, stub_type, ext )
-                file_path = os.path.join( series_root, file_name )
+        if self.ARGS.streamfile:
+            ext = 'strm'
+        else:
+            ext = 'disc'
+        if self.ARGS.fromsettings:
+            pass
+        else:
+            ref = 0
+            for season in season_list:
+                season_num = self._add_leading_zeros( season )
+                try:
+                    max_eps = int( episode_list[ref] ) + 1
+                except IndexError:
+                    break
+                for e in range( 1, max_eps):
+                    ep_num = self._add_leading_zeros( e )
+                    file_name = '%s.S%sE%s%s.%s' % (video_name, season_num, ep_num, media_type, ext )
+                    file_path = os.path.join( video_path, file_name )
+                    success, loglines = writeFile( file_text, file_path, 'w' )
+                    lw.log( loglines )
+                    if success and date_list:
+                        t = time.mktime(time.strptime(date_list[ref], config.Get( 'dateformat' )))
+                        os.utime(file_path, (t,t))
+                ref = ref + 1
+            if not season_list:
+                file_name = '%s%s.%s' % (video_name, media_type, ext )
+                file_path = os.path.join( video_path, file_name )
                 success, loglines = writeFile( file_text, file_path, 'w' )
                 lw.log( loglines )
-                if success and date_list:
-                    t = time.mktime(time.strptime(date_list[ref], config.Get( 'dateformat' )))
-                    os.utime(file_path, (t,t))
-            ref = ref + 1
 
 
     def _get_file_text( self ):
-        if self.ARGS.title:
-            title = self.ARGS.title
-        else:
-            title = ''
-        if self.ARGS.msg:
-            message = self.ARGS.msg
-        else:
-            message = ''
-        if title or message:
+        if self.TITLE or self.MSG:
             if self.ARGS.streamfile:
-                return 'plugin://plugin.whereareyou?empty=pad&%s' % urlencode( {'title':title, 'message':message}, quote_via=quote_plus )
+                return 'plugin://plugin.whereareyou?empty=pad&%s' % urlencode( {'title':self.TITLE, 'message':self.MSG}, quote_via=quote_plus )
             else:
-                replacement_dic = {'[TITLE]' : title,
-                                   '[MESSAGE]' : message}
-                loglines, template = readFile( os.path.join( self.DATAROOT, 'disc_template.txt' ) )
-                lw.log (loglines )
-                return replaceWords( template, replacement_dic )
+                return '<discstub>\r    <title>%s</title>\r    <message>%s</message>\r</discstub>' % (self.TITLE, self.MSG)
         else:
             return ''
 
