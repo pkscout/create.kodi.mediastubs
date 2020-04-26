1021ea8247974e7920c93c843e226a99068be258
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2020-04-25 17:26:49 -1000

    code cleanup and common lib updates

--- resources/lib/fileops.py
+++ resources/lib/fileops.py
@@ -124,12 +124,12 @@ def atoi( text ):
     return int(text) if text.isdigit() else text
 
 
-def naturalKeys( text ):
+def naturalKeys( thelist ):
     '''
     alist.sort( key=naturalKeys ) sorts in human order
     http://nedbatchelder.com/blog/200712/human_sorting.html
     '''
-    return [ atoi( c ) for c in re.split( r'(\d+)', text ) ]
+    return [ atoi( c ) for c in re.split( r'(\d+)', thelist ) ]
 
 
 def readFile( filename ):
