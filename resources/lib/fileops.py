f036d1cc7a6111c5d539bf072bbb6a9213f3fea6
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2020-05-24 12:01:43 -1000

    updated commonlibs

--- resources/lib/fileops.py
+++ resources/lib/fileops.py
@@ -1,10 +1,6 @@
-# v.0.9.0
+# v.0.12.0
 
-import os, re
-try:
-    _range = range
-except NameError:
-    _range = xrange
+import os, re, sys
 try:
     from kodi_six import xbmcvfs
     isXBMC = True
@@ -12,22 +8,22 @@ except ImportError:
     isXBMC= False
 
 if isXBMC:
-    _mkdirs = xbmcvfs.mkdirs
-    _rmdir  = xbmcvfs.rmdir
-    _exists = xbmcvfs.exists
-    _delete = xbmcvfs.delete
-    _copy   = xbmcvfs.copy
-    _open   = xbmcvfs.File
-    _rename = xbmcvfs.rename
+    _mkdirs  = xbmcvfs.mkdirs
+    _rmdir   = xbmcvfs.rmdir
+    _exists  = xbmcvfs.exists
+    _delete  = xbmcvfs.delete
+    _copy    = xbmcvfs.copy
+    _open    = xbmcvfs.File
+    _rename  = xbmcvfs.rename
 else:
     import shutil
-    _mkdirs = os.makedirs
-    _rmdir  = os.rmdir
-    _exists = os.path.exists
-    _delete = os.remove
-    _copy   = shutil.copyfile
-    _open   = open
-    _rename = os.rename
+    _mkdirs  = os.makedirs
+    _rmdir   = os.rmdir
+    _exists  = os.path.exists
+    _delete  = os.remove
+    _copy    = shutil.copyfile
+    _open    = open
+    _rename  = os.rename
 
 
 def checkPath( thepath, createdir=True ):
@@ -48,8 +44,8 @@ def checkPath( thepath, createdir=True ):
 def copyFile( thesource, thedest ):
     log_lines = []
     if _exists( thesource ):
+        log_lines.append( 'copying file %s to %s' % (thesource, thedest) )
         try:
-            log_lines.append( 'copying file %s to %s' % (thesource, thedest) )
             _copy( thesource, thedest )
         except IOError:
             log_lines.append( 'unable to copy %s to %s' % (thesource, thedest) )
@@ -83,8 +79,8 @@ def deleteFolder( thesource, thetype='folder' ):
             _action = _rmdir
         else:
             _action = _delete
+        log_lines.append( 'deleting %s %s' % (thetype, thesource) )
         try:
-            log_lines.append( 'deleting %s %s' % (thetype, thesource) )
             if isXBMC:
                 if not _action( thesource ):
                     raise IOError( 'unable to delete item' )
@@ -101,6 +97,45 @@ def deleteFolder( thesource, thetype='folder' ):
     else:
         log_lines.append( '%s does not exist' % thesource )
         return False, log_lines
+
+
+def listDirectory( thesource, thefilter='all' ):
+    log_lines = []
+    log_lines.append( 'getting contents of folder %s' % thesource )
+    if isXBMC:
+        try:
+            dirs, files = xbmcvfs.listdir( thesource )
+        except OSError:
+            log_lines.append( 'OSError getting directory list' )
+            return [], log_lines
+        except Exception as e:
+            log_lines.append( 'unexpected error getting directory list' )
+            log_lines.append( e )
+            return [], log_lines
+        if thefilter == 'files':
+            log_lines.append( 'returning files from %s' % thesource )
+            log_lines.append( files )
+            return files, log_lines
+        elif thefilter =='folders':
+            log_lines.append( 'returning folders from %s' % thesource )
+            log_lines.append( dirs )
+            return dirs, log_lines
+        else:
+            log_lines.append( 'returning files and folders from %s' % thesource )
+            log_lines.append( files + dirs )
+            return files + dirs, log_lines
+    else:
+        try:
+            contents = os.listdir( thesource )
+        except OSError:
+            log_lines.append( 'OSError getting directory list' )
+            return [], log_lines
+        except Exception as e:
+            log_lines.append( 'unexpected error getting directory list' )
+            log_lines.append( e )
+            return [], log_lines
+        log_lines.append( 'returning files and folders from %s' % thesource )
+        return contents, log_lines
 
 
 def moveFile( thesource, thedest ):
@@ -120,16 +155,13 @@ def moveFile( thesource, thedest ):
     return success, log_lines + cp_loglines + dl_loglines
 
 
-def atoi( text ):
+def _atoi( text ):
     return int(text) if text.isdigit() else text
 
 
 def naturalKeys( thelist ):
-    '''
-    alist.sort( key=naturalKeys ) sorts in human order
-    http://nedbatchelder.com/blog/200712/human_sorting.html
-    '''
-    return [ atoi( c ) for c in re.split( r'(\d+)', thelist ) ]
+    # from http://nedbatchelder.com/blog/200712/human_sorting.html
+    return [ _atoi( c ) for c in re.split( r'(\d+)', thelist ) ]
 
 
 def osPathFromString( spath, sep='/' ):
@@ -143,10 +175,14 @@ def osPathFromString( spath, sep='/' ):
 def readFile( filename ):
     log_lines = []
     if _exists( filename ):
-        thefile = _open( filename, 'r' )
         try:
-            thedata = thefile.read()
-            thefile.close()
+            if sys.version_info >= (3, 0):
+                with _open( filename, 'r') as thefile:
+                    thedata = thefile.read()
+            else:
+                thefile = _open( filename, 'r' )
+                thedata = thefile.read()
+                thefile.close()
         except IOError:
             log_lines.append( 'unable to read data from ' + filename )
             return log_lines, ''
@@ -162,8 +198,8 @@ def readFile( filename ):
 
 def renameFile ( thesource, thedest ):
     log_lines = []
+    log_lines.append( 'renaming file %s to %s' % (thesource, thedest) )
     try:
-        log_lines.append( 'renaming file %s to %s' % (thesource, thedest) )
         _rename( thesource, thedest )
     except IOError:
         log_lines.append( 'unable to rename %s to %s' % (thesource, thedest) )
@@ -175,14 +211,42 @@ def renameFile ( thesource, thedest ):
     return True, log_lines
 
 
+def _remove_trailing_dot( thename, endreplace='' ):
+    if thename[-1] == '.' and len( thename ) > 1 and endreplace is not '.':
+        return _remove_trailing_dot( thename[:-1] + endreplace )
+    else:
+        return thename
+
+
+def setSafeName( thename, illegalchars='<>:"/\|?*', illegalreplace='_', endreplace='' ):
+    loglines = []
+    if not thename:
+        loglines.append( 'name is empty, returning it' )
+        return thename, loglines
+    s_name = ''
+    loglines.append( 'started with %s' % thename )
+    loglines.append( 'the illegal characters are %s and the replacement is %s' % (illegalchars, illegalreplace) )
+    for c in list( _remove_trailing_dot( thename, endreplace=endreplace ) ):
+        if c in illegalchars:
+            s_name = s_name + illegalreplace
+        else:
+            s_name = s_name + c
+    loglines.append( 'finished with %s' % s_name )
+    return s_name, loglines
+        
+
 def writeFile( data, filename, wtype='wb' ):
     log_lines = []
     if type(data).__name__=='unicode':
         data = data.encode('utf-8')
-    thefile = _open( filename, wtype )
     try:
-        thefile.write( data )
-        thefile.close()
+        if sys.version_info >= (3, 0):
+            with _open( filename, wtype) as thefile:
+                thefile.write( data )
+        else:
+            thefile = _open( filename, wtype )
+            thefile.write( data )
+            thefile.close()
     except IOError as e:
         log_lines.append( 'unable to write data to ' + filename )
         log_lines.append( e )
